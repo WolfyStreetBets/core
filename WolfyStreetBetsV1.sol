2214e508c170dda3cc429c13377125364d854289
WolfyStreetBets <81118224+WolfyStreetBets@users.noreply.github.com>
2021-08-18 13:58:30 +0100

    removed unused return param

--- WolfyStreetBetsV1.sol
+++ WolfyStreetBetsV1.sol
@@ -1090,8 +1090,8 @@ contract WolfyStreetBetsV1 is Ownable, ReentrancyGuard, BaseRelayRecipient {
     * @return LRLiq user liquidity in low risk pool (uint256)
     * @return HRLiq user liquidity in high risk pool (uint256)
     */
-    function getLiquidityStatsPerUser() external view returns (uint256 LPFeesCollected, uint256 LRLiq, uint256 HRLiq, uint256 totalLiq) {
-        return (userLPReward[_msgSender()].div(10**decimals()), currentLowLiquidity[_msgSender()], currentHighLiquidity[_msgSender()], currentLowLiquidity[_msgSender()].add(currentHighLiquidity[_msgSender()]));
+    function getLiquidityStatsPerUser() external view returns (uint256 LPFeesCollected, uint256 LRLiq, uint256 HRLiq) {
+        return (userLPReward[_msgSender()].div(10**decimals()), currentLowLiquidity[_msgSender()], currentHighLiquidity[_msgSender()]);
     }
 
     /**
